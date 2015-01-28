e4e376fe3c95efca761329e16c3a75c0ef611280
ken <ken.mc@email.me>
2015-01-28 10:07:42 +0000

    trying to get ftp working on new site

--- src/pywws/Upload.py
+++ src/pywws/Upload.py
@@ -92,11 +92,15 @@ class _ftp(object):
         global ftplib
         import ftplib
         self.logger = logger
-        self.site = site
-        self.user = user
-        self.password = password
-        self.directory = directory
-        self.port = port
+        #self.site = site
+        #self.user = user
+        #self.password = password
+        #self.directory = directory
+        #self.port = port
+        self.site="goatstownweather.eu.pn"
+        self.user="1801145"
+        self.password="thehitha1"
+        self.directory="/home/www"
 
     def connect(self):
         self.logger.info("Uploading to web site with FTP")

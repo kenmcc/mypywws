bb6276d5616204925e2267f2bb57695ecefaf5d7
ken <ken@weatherpi>
2015-03-03 23:13:40 +0000

    updated to retry on failure

--- src/batteryPlotter.py
+++ src/batteryPlotter.py
@@ -1,6 +1,7 @@
 import sqlite3
+import time
 
-con = sqlite3.connect("weather.db")
+con = sqlite3.connect("/data/weather.db")
 con.isolation_level = None
 cur = con.cursor()
 
@@ -8,12 +9,20 @@ cur = con.cursor()
 NODENAMES = {"2":"PTH","3":"Rain","10":"Kitchen", "21":"Bedroom"}
 
 nodes = []
-d = cur.execute("select distinct node from data")
+success = False
+while not success:
+  try:
+    d = cur.execute("select distinct node from data")
+    success = True
+  except:
+    print "failed to get nodes, sleeping"
+    time.sleep(10)
+
 for x in d:
     nodes.append(x[0])
     
 for x in nodes:
-   with open("/tmp/battery_{0}.txt".format(x), "a+") as f:
+   with open("/ramtemp/battery_{0}.txt".format(x), "a+") as f:
        existingData = f.readlines()
        dateQualifier = ""
        lastDate = ""
@@ -23,7 +32,15 @@ for x in nodes:
     
        statement = "select t.date, t.batt from data t join(select max(tt.date) 'maxtimestamp' from data tt where node={0} {1} group by date(tt.date)) m on m.maxtimestamp = t.date".format(x, dateQualifier)
        #print statement
-       d = cur.execute(statement)
+       success = False
+       while not success:
+          try:
+             d = cur.execute(statement)
+             success = True
+          except:
+             print "failed to execute query, sleeping"
+             time.sleep(10)
+
        for y in d:
            date = y[0].split(" ")[0]
            val = str(y[1])
@@ -31,9 +48,9 @@ for x in nodes:
                f.write("{0}\t{1}\n".format(date, val))
         
         
-with open("/tmp/plotter.sh", "w") as f:
+with open("/ramtemp/plotter.sh", "w") as f:
     f.write("set term png\n")
-    f.write("set output 'battery_levels.png'\n")
+    f.write("set output '/ramtemp/battery_levels.png'\n")
     f.write("set xdata time\n")
     f.write("set timefmt '%Y-%m-%d'\n")
     f.write("set format x '%d/%m'\n")
@@ -41,7 +58,7 @@ with open("/tmp/plotter.sh", "w") as f:
     plots = []
     plotnum = 1
     for x in nodes:
-        plots.append('"battery_{0}.txt" using 1:2 title "{2}" smooth unique lc {1} lw 1'.format(x, plotnum, NODENAMES[str(x)]+"("+str(x)+")" if str(x) in NODENAMES else x))
+        plots.append('"/ramtemp/battery_{0}.txt" using 1:2 title "{2}" smooth unique lc {1} lw 1'.format(x, plotnum, NODENAMES[str(x)]+"("+str(x)+")" if str(x) in NODENAMES else x))
         plotnum += 1
     f.write("plot " + ",".join(plots) + "\n")
     

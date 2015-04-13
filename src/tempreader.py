a23902c6d1d87b2dc1ae910ebdab232f755cd850
ken <ken@weatherpi>
2015-04-13 21:33:11 +0100

    added logging module

--- src/tempreader.py
+++ src/tempreader.py
@@ -5,20 +5,38 @@ import datetime
 from datalogger import *
 import array, fcntl, sys
 
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+#logging.basicConfig(format='%(asctime)s %(message)s', filename='/data/logs/tempreader.log',level=logging.DEBUG)
+log = logging.getLogger("TempReader")
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+handler = TimedRotatingFileHandler("/data/logs/tempreader.log",
+                                       when="d",
+                                       interval=1,
+                                       backupCount=7)
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
 fd = os.open("/dev/rfm12b.0.1",  os.O_NONBLOCK|os.O_RDWR)
 localURI="http://192.168.1.31/emoncms/input/post.json?apikey=d959950e0385107e37e2457db27b781e&node="
 remoteURI="http://emoncms.org/input/post.json?apikey=a6958b2d85dfdfab9406e1e786e38249&node="
 goatstownURI="http://goatstownweather.hostoi.com/emoncms/input/post.json?apikey=0f170b829035f4cde06637f953852333&node="
 
+def dbgPrint(theString):
+   log.info(theString)
+   print theString
+
 if len(sys.argv)== 2:
    if sys.argv[1] == "9600":
-     print "Setting baudrate to 9600"
+     dbgPrint("Setting baudrate to 9600")
      buf = array.array('h',[0x23])
      fcntl.ioctl(fd, 1074033160, buf, 1)
 
      fcntl.ioctl(fd, -2147192317, buf, 1)
      if buf[0] == 35:
-        print "Successfully"
+        dbgPrint("Successfully")
 
 logger = dataLogger("/data/weather.db")
 fileLogger = fileDataLogger("/data/weatherdata")
@@ -41,7 +59,7 @@ while run == True:
                     {"field": "humidity", "value": str(int(humidity)/100)})
           logger.insert(fields)
           fileLogger.insert(fields)
-          print "Temp {0}, batt {1}, pressure {2}, humidity{3}".format(temp, batt, pressure, humidity)
+          dbgPrint("Temp {0}, batt {1}, pressure {2}, humidity{3}".format(temp, batt, pressure, humidity))
 
         elif node == 3 and len == 10:
           rain,batt,a,b,c = struct.unpack("hhhhh", data[2:])
@@ -50,10 +68,10 @@ while run == True:
                     {"field": "rain", "value": str(float(rain)/100)})
           logger.insert(fields)
           fileLogger.insert(fields)
-          print "Rain {0}, batt {1}".format(rain, batt)
+          dbgPrint("Rain {0}, batt {1}".format(rain, batt))
 
         elif node == 9 and len == 16:
-          print "WH1080"
+          dbgPrint("WH1080")
           temp,batt,humidity, wind_avg, wind_gust,wind_dir, rain = struct.unpack("ihhhhhh", data[2:])
           temp=temp/10.0
           fields = ({"field": "node", "value": str(node)}, 
@@ -65,15 +83,14 @@ while run == True:
                     {"field": "temp", "value": str(temp)}, 
                     {"field": "rain", "value": str(float(rain)/10)}
                     )
-          print "Logging"
 	  logger.insert(fields)
           fileLogger.insert(fields)
-          print "WH1080:", temp, batt, humidity, wind_avg, wind_gust, wind_dir, rain
+          dbgPrint("WH1080:", temp, batt, humidity, wind_avg, wind_gust, wind_dir, rain)
  
         elif node >= 10 and node < 20 and len == 4:
                 temp,batt = struct.unpack("hh", data[2:])
                 if temp > -2000 and temp < 4000:
-                  print "Got node, temp, batt = ", node, temp, batt
+                  dbgPrint("Got node {0}, temp {1}, batt {2} ".format(node, temp, batt))
                   fields = ({"field": "node", "value": str(node)}, 
                             {"field": "batt", "value": str(float(batt)/1000)}, 
                             {"field": "temp", "value": str(float(temp)/100)})
@@ -81,10 +98,10 @@ while run == True:
                   fileLogger.insert(fields)
                   jsonStr = "temp:"+str(float(temp/100.0))+",batt:"+str(float(batt/1000.0))
         elif node == 20 and len == 8: # this is a pressure sensor 
-          print "pressure sensor"
+          dbgPrint("pressure sensor")
           temp, batt, pressure = struct.unpack("hhi", data[2:])
           if temp >-2000 and temp < 40000 and pressure >900 and pressure < 1100:
-                 print "Got temp, battery, pressure = ", temp, batt, pressure
+                 dbgPrint("Got temp {0}, battery {1}, pressure {2} ".format( temp, batt, pressure))
                  jsonStr = "temp:"+str(float(temp/10.0))
                  jsonStr += ",batt:"+str(float(batt/1000.0))
                  jsonStr += ",pressure:"+str(pressure)  
@@ -103,11 +120,11 @@ while run == True:
               pass
 
         else:
-          print "don't know what to do with node, len", node, len
+          dbgPrint("don't know what to do with node {0}, len {1}".format(node, len))
           if node == 3 and len == 10:
                 rain, batt, a, b, c = struct.unpack("hhhhh", data[2:])
-                print "Batt = ", batt, "rain=", rain                
+                dbgPrint("Batt = " + batt + "rain=" +  rain)
 
     except Exception, e:
-        print "Exception ", e
+        dbgPrint("Exception {0}".format(str(e)))
 os.close(fd)

6afa5344bba928e2a256f98560129940cf4a5ce2
Ken McCullagh <ken.mccullagh@accenture.com>
2016-09-01 10:12:15 +0100

    added a summary to the battery values

--- src/batteryplot.sh
+++ src/batteryplot.sh
@@ -9,5 +9,7 @@ python -m pywws.Upload /data/weatherdata /ramtemp/battery_2.txt
 python -m pywws.Upload /data/weatherdata /ramtemp/battery_10.txt
 python -m pywws.Upload /data/weatherdata /ramtemp/battery_3.txt
 python -m pywws.Upload /data/weatherdata /ramtemp/battery_21.txt
+python -m pywws.Upload /data/weatherdata /ramtemp/battery_summary.txt
+
 
 
